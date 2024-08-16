c0353e99061e632dc839391b1d04f6397adc6b3e
andrewn9 <ng9417265@gmail.com>
2024-08-16 17:48:24 -0400

    Added main menu scene

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public static int level;
+    public static int highest_level;
+    public static bool hasSave;
+  
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
