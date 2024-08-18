a4203bd344a5c5e7dba04718405fb8f7bf6b0590
nullishew <155263075+nullishew@users.noreply.github.com>
2024-08-17 23:14:17 -0400

    Update ScaleBehavior.cs
    Fixed absorption behavior:
    - Only 3 mass can be absorbed
    - Fixed bug where blocks of mass less than 3 get reabsorbed during death animation

--- Assets/Scripts/ScaleBehavior.cs
+++ Assets/Scripts/ScaleBehavior.cs
@@ -46,6 +46,9 @@ public class ScaleBehavior : MonoBehaviour
         Debug.Log("Mass on top " + mass_ontop);
         down = mass_ontop > 0;
         animator.SetBool("Down", down);
+
+        Absorb();
+
     }
 
 
@@ -94,20 +97,22 @@ public class ScaleBehavior : MonoBehaviour
             var body = hit.collider.attachedRigidbody;
 
             if (body == null || body == _rigidbody || body.bodyType == RigidbodyType2D.Static) continue;
+            if (body.CompareTag("dying")) continue;
 
             int mass = Mathf.Max(0, AbsorbableMass - Mass);
             if (mass == 0) continue;
             AudioManager.PlaySound("absorb");
 
             mass = Mathf.Min(mass, (int) body.mass);
-
+            //Debug.Log("Mass");
             Mass += mass;
-            body.tag = "dying";
             down = false;
             if (body.mass - mass == 0)
             {
                 //body.GetComponent<Animator>().SetTrigger("Die");
                 //Destroy(body.gameObject);
+                body.tag = "dying";
+
                 continue;
             }
             body.mass -= mass;
@@ -143,9 +148,9 @@ public class ScaleBehavior : MonoBehaviour
             }
         };
 
-        InputSystem.actions.FindAction("Absorb").started += (context) =>
-        {
-            Absorb();
-        };
+        //InputSystem.actions.FindAction("Absorb").started += (context) =>
+        //{
+        //    Absorb();
+        //};
     }
 }
