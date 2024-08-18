d3fe73c84d4815adfc895fa0cf694f94ff86368e
nullishew <155263075+nullishew@users.noreply.github.com>
2024-08-17 20:31:32 -0400

    Added button and cleaned up prefabs
    Added animated button that activates target tiles when pressed
    Removed broken prefabs
    Changed the source of the arrow tile images from the individually stored images to the spritesheet images
    Removed unused images
    Organized prefabs into correct folders

---
+++ Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    [SerializeField] private List<Rigidbody2D> _targets = new();
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Rigidbody2D rigidbody))
+        {
+            //Debug.Log("deez nuts");
+            _animator.ResetTrigger("Release");
+            _animator.SetTrigger("Press");
+            // activate anim
+            foreach (var target in _targets)
+            {
+                // do the thing
+
+            }
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _animator.ResetTrigger("Press");
+        _animator.SetTrigger("Release");
+
+    }
+}
