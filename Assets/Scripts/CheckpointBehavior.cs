ac545ea47b766668b6ec171f534909cefba67815
andrewn9 <ng9417265@gmail.com>
2024-08-19 20:43:28 -0400

    bonus level

--- Assets/Scripts/CheckpointBehavior.cs
+++ Assets/Scripts/CheckpointBehavior.cs
@@ -33,7 +33,7 @@ public class CheckpointBehavior : MonoBehaviour
 
             yield return new WaitForSeconds(0.65f);
         
-            if (GameManager.level < 13)
+            if (GameManager.level < 15)
             {
                 SceneManager.LoadScene("Level " + (GameManager.level + 1), LoadSceneMode.Single);
             }
